c98443c61c1ef572f0f523ffd6d64fc33f6adf45
yuan310979 <chc71340@gmail.com>
2018-08-15 00:07:03 +0800

    configure with input dataset

--- hw01-association_rule/Apriori.java
+++ hw01-association_rule/Apriori.java
@@ -1,4 +1,6 @@
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Apriori{
@@ -9,9 +11,15 @@ public class Apriori{
     public static void main(String[] args) throws Exception{
         Apriori ap = new Apriori(args);
     } 
-    
+
     /** the list of current itemsets */
     private List<int[]> itemsets;
+    /** the set of all items */
+    private HashSet<Integer> items;
+    /** total number of transactions */
+    private int numTrans;
+    /** total number of items*/
+    private int numItems;
     /** the transaction filename */
     private String transFilename;
     /** minimum support */
@@ -22,14 +30,48 @@ public class Apriori{
     public Apriori(String[] args){
         configure(args);
     }
-    
+
     private void configure(String[] args){
         if(args.length != 0)    transFilename = args[0]; 
         if(args.length >= 2)    minSup = Double.parseDouble(args[1]);
         else                    minSup = 0.8;
         if(args.length == 3)    minConf = Double.parseDouble(args[2]);
         else                    minConf = 0.8;
-        
+
+        BufferedReader br = null;
+        FileReader fr = null;
+
+        try{
+            fr = new FileReader(transFilename);
+            br = new BufferedReader(fr);
+            items = new HashSet<Integer>();
+
+            String tran;
+
+            while((tran = br.readLine()) != null){
+                // ignore empty line
+                if(tran.matches("\\s*"))    continue;
+                numTrans++;
+                String[] tranStrArray = tran.replaceAll("\\s+", "").split(",");
+                for(int i = 0; i < tranStrArray.length; i++){
+                    items.add(Integer.parseInt(tranStrArray[i]));
+                } 
+            }
+            numItems = items.size();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        log("=============== Configuration ===============");
+        log("Dataset name: " + transFilename);
+        log("Total transactions: " + numTrans);
+        log("Total items: " + numItems);
+        log("=========================================");
+    }
+
+    /** print out some messages */
+    private void log(String str){
+        System.err.println(str);
     }
 }
 
